2e747a17feff39f93568449fc420838775efbe3e
Pichawat A <pichawat43695@gmail.com>
2023-03-07 18:28:54 +0700

    During Report Lab 4

--- Lab 4-2.c
+++ Lab 4-2.c
@@ -73,7 +73,7 @@ void createQueue(queue *q)
 
 int isempty(queue *q)
 {
-    return (q->rear == NULL);
+    return (q->rear == NULL); // Return statement If rear == NULL
 }
 
 void enqueue(queue *q, char *value)
@@ -84,12 +84,12 @@ void enqueue(queue *q, char *value)
         tmp = malloc(sizeof(t_node));
         tmp->data = value;
         tmp->next = NULL;	
-        if(!isempty(q))
+        if(!isempty(q)) // Queue is not empty insert temp node at front.
         {
             q->rear->next = tmp;
             q->rear = tmp;
         }
-        else
+        else // Queue is empty point front and rear at temp node.
         {
             q->front = q->rear = tmp;
         }
@@ -97,7 +97,7 @@ void enqueue(queue *q, char *value)
     }
     else
     {
-        printf("LIST IS FULL\n");
+        printf("Queue is full.\n");
     }
 }
 
@@ -105,7 +105,7 @@ char *dequeue(queue *q)
 {
     t_node *tmp; 
     tmp = q->front;
-    char *n = q->front->data;
+    char *n = q->front->data; // Collect front node data
     q->front = q->front->next;
     q->count--;
     free(tmp);
@@ -115,20 +115,22 @@ char *dequeue(queue *q)
 void display(queue *q,int delNum)
 {
     int lastNode = 0;
-    if(q->front == NULL)
+    if(q->front == NULL) // If queue is NULL print None then exit the function.
     {
         printf("None\n");
         exit(0);
     }
     while(delNum != 0)
     {
-        if(q->front->next == NULL && lastNode == 0)
+        if(q->front->next == NULL && lastNode == 0) // There is still up coming node and it isn't the last node
         {
-            q->front->data[strcspn(q->front->data,"\n")] = 0; // Remove newline character from last node.
+            // Remove newline character from last node. By searching for \n 
+            // in data string then return the index of \n. Then replace it with NULL characters.
+            q->front->data[strcspn(q->front->data,"\n")] = '\0'; 
             printf("%s ",q->front->data);
             lastNode += 1;
         }
-        else if(lastNode >= 1)
+        else if(lastNode >= 1) // If it is the last node or overcall node.
         {
             printf("None ");
         }
